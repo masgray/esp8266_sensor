9213ad26d155df55c387a7d47f75b317ab0706a3
Sergey Maslov <masgray@mail.ru>
2017-08-30 23:02:58 +0300

    Working version

--- esp_sensors/esp_sensors.ino
+++ esp_sensors/esp_sensors.ino
@@ -7,15 +7,20 @@
 const char* MqttServer = "192.168.0.3";
 const uint16_t MqttPort = 1883;
 
-const char* Device1 = "Device1";
-const char* ResetTopic = "Reset";
+const char* Device1 = "unit1_device1";
+const char* ResetTopic = "unit1/device1/reset/action";
+constexpr const char *Sensor1 = "unit1/device1/sensor1/status";
+constexpr const char *Sensor2 = "unit1/device1/sensor2/status";
+constexpr const char *Sensor3 = "unit1/device1/sensor3/status";
+constexpr const char *Error = "unit1/device1/error/status";
+constexpr const char *Hello = "unit1/device1/hello/status";
 
 void MqttCallback(char* topic, byte* payload, unsigned int length);
 
 WiFiClient espClient;
 PubSubClient mqtt(MqttServer, MqttPort, MqttCallback, espClient);
 
-static BME280I2C bme;
+BME280I2C bme;
 float temperature = NAN;
 float humidity = NAN;
 float pressure = NAN;
@@ -26,7 +31,7 @@ void setup()
   Serial.begin(115200);
   while(!Serial) 
   {} // Wait
-  while(!bme.begin())
+  while(!bme.begin(5, 4))
   {
     Serial.println("Could not find BME280 sensor on I2C bus!");
     delay(1000);
@@ -60,32 +65,36 @@ void loop()
   }
   mqtt.loop();
 
-  bme.read(pressure, temperature, humidity, true, 2);
+  bme.read(pressure, temperature, humidity);
   if (pressure == NAN)
   {
     Serial.println("Error reading sensors data!");
-    mqtt.publish("Error", "1");
+    mqtt.publish(Error, "1");
   }
   else
   {
     Serial.print("Temperature: ");
     Serial.print(temperature);
-    Serial.print("°C");
+    Serial.print("'C");
     Serial.print("\t\tHumidity: ");
     Serial.print(humidity);
     Serial.print("% RH");
     Serial.print("\t\tPressure: ");
+    pressure = pressure / 133.3;
     Serial.print(pressure);
-    Serial.println(" Hg");
+    Serial.println(" mm Hg");
 
-    mqtt.publish("Error", "0");
+    mqtt.publish(Error, "0");
     static char msg[32];
-    snprintf(msg, 31, "%0.2f", temperature);
-    mqtt.publish("Sensor1", msg);
-    snprintf(msg, 31, "%0.2f", humidity);
-    mqtt.publish("Sensor2", msg);
-    snprintf(msg, 31, "%0.2f", pressure);
-    mqtt.publish("Sensor3", msg);
+    
+    dtostrf(temperature, 4, 2, msg);
+    mqtt.publish(Sensor1, msg);
+    
+    dtostrf(humidity, 4, 2, msg);
+    mqtt.publish(Sensor2, msg);
+    
+    dtostrf(pressure, 4, 1, msg);
+    mqtt.publish(Sensor3, msg);
   }
   delay(1000);
 }
@@ -126,7 +135,7 @@ void MqttReconnect()
       ++reconnectCounter;
       static char msg[32];
       snprintf(msg, 31, "%ld", reconnectCounter);
-      mqtt.publish("Hello", msg);
+      mqtt.publish(Hello, msg);
       // ... and resubscribe
       mqtt.subscribe(ResetTopic);
     } 
